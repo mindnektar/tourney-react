1f927f71a8b5862d586a4bf928ff31cf95f58290
Martin Denk <wyverns.spirit@gmail.com>
2016-04-15 10:17:10 +0200

    awesome knockout positioning

--- js/components/App/Content/Knockout.js
+++ js/components/App/Content/Knockout.js
@@ -4,15 +4,18 @@ import Round from './Knockout/Round';
 class Knockout extends Component {
     render() {
         return (
-            <div className="column-container">
-                {this.props.rounds && this.props.rounds.map((round, roundIndex) =>
-                    <Round
-                        changeScore={(matchIndex, playerIndex, gameIndex, score) => this.props.changeScore(roundIndex, matchIndex, playerIndex, gameIndex, score)}
-                        key={roundIndex}
-                        label={roundIndex === this.props.rounds.length - 1 ? 'Final round' : `Knock-out round ${roundIndex + 1}`}
-                        matches={round}
-                    />
-                )}
+            <div className="knockout">
+                <h2>Knock-out</h2>
+
+                <div className="column-container">
+                    {this.props.rounds && this.props.rounds.map((round, roundIndex) =>
+                        <Round
+                            changeScore={(matchIndex, playerIndex, gameIndex, score) => this.props.changeScore(roundIndex, matchIndex, playerIndex, gameIndex, score)}
+                            key={roundIndex}
+                            matches={round}
+                        />
+                    )}
+                </div>
             </div>
         );
     }
--- js/components/App/Content/Knockout/Round.js
+++ js/components/App/Content/Knockout/Round.js
@@ -4,8 +4,6 @@ class Round extends Component {
     render() {
         return (
             <div className="column">
-                <h2>{this.props.label}</h2>
-
                 {this.props.matches.map((match, matchIndex) =>
                     <table key={matchIndex}>
                         <tbody>
@@ -40,7 +38,6 @@ class Round extends Component {
 
 Round.propTypes = {
     changeScore: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired,
     matches: PropTypes.array.isRequired,
 };
 
